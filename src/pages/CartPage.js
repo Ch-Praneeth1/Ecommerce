26071901a9d12e9684b255facea2599af717e946
Ch-Praneeth1 <chpraneethvardhanreddy143@gmail.com>
2024-01-21 21:26:53 +0530

    Added nav-bar in cart page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Cart from '../features/cart/Cart'
-
+import {Navbar} from '../features/nav-bar/Navbar'
 const CartPage = () => {
   return (
     <div>
+      <Navbar>
         <Cart></Cart>
+      </Navbar>
     </div>
   )
 }
